0141bfe1a0e9528c60b7584c9405bbe12ba64a04
kovacspe <pet.kovacs1@gmail.com>
2022-09-17 20:54:46 +0200

    Add urls for registration

--- competition/urls.py
+++ competition/urls.py
@@ -5,5 +5,9 @@ from . import views
 app_name = 'competition'
 
 urlpatterns = [
-    path('registracia', views.SignUpView.as_view(), name='registration')
+    path('registracia', views.SignUpView.as_view(), name='registration'),
+    path('prihlasenie', views.LoginFormView.as_view(), name='login'),
+    path('odhlasenie', views.logout_view, name='logout'),
+    path('sutaz', views.GameView.as_view(), name='game'),
+    path('zmena-hesla', views.change_password, name='change-password')
 ]
