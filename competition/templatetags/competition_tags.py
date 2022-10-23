5f8177d88c4531063642e4911a204c6ff592e65d
kovacspe <pet.kovacs1@gmail.com>
2022-10-23 21:34:15 +0200

    Add can_submit tag

--- competition/templatetags/competition_tags.py
+++ competition/templatetags/competition_tags.py
@@ -7,3 +7,7 @@ register = template.Library()
 @register.simple_tag
 def number_of_solved(level, competitor):
     return level.number_of_solved(competitor)
+
+@register.simple_tag
+def can_submit(problem, competitor):
+    return problem.can_submit(competitor)
