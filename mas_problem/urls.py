ce179f5ba12a436252b85956599864e0212634b6
kovacspe <pet.kovacs1@gmail.com>
2022-10-25 22:26:47 +0200

    Pridanie zmeny hesla

--- mas_problem/urls.py
+++ mas_problem/urls.py
@@ -1,10 +1,31 @@
-from allauth.account.views import confirm_email
+import allauth.account.views as allauth_views
 from django.contrib import admin
 from django.urls import include, path, re_path
+
+from competition.views import LoginFormView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('competition.urls', namespace='competition')),
-    re_path(r'^accounts/confirm-email/(?P<key>[-:\w]+)/$', confirm_email, name='account_confirm_email'),
+    
+    # Allauth
+    re_path(r'^accounts/confirm-email/(?P<key>[-:\w]+)/$', allauth_views.confirm_email, name='account_confirm_email'),
+    path("password/reset/", allauth_views.password_reset, name="account_reset_password"),
+    path(
+        "password/reset/done/",
+        allauth_views.password_reset_done,
+        name="account_reset_password_done",
+    ),
+    re_path(
+        r"^password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$",
+        allauth_views.password_reset_from_key,
+        name="account_reset_password_from_key",
+    ),
+    path(
+        "password/reset/key/done/",
+        allauth_views.password_reset_from_key_done,
+        name="account_reset_password_from_key_done",
+    ),
+    path('login',LoginFormView.as_view(),name='account_login')
 ]
 handler404 = 'competition.views.view_404'
