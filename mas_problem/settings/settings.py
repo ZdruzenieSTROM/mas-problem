7128401420e271a70174bc6b750e011bcd31bee0
kovacspe <pet.kovacs1@gmail.com>
2022-11-07 23:20:32 +0100

    media path

--- mas_problem/settings/settings.py
+++ mas_problem/settings/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
-MEDIA_URL = '/media/' 
+MEDIA_URL = 'media/' 
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
