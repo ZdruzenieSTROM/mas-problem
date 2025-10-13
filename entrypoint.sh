ef86e57f2b36d4a8ad1bcff023b9e43b724c70c3
Smilinko <martin.smilnak@gmail.com>
2025-10-13 22:43:56 +0200

    forcing database migrations on deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@
 echo "Collect static files"
 python manage.py collectstatic --noinput
 
+# Make database migrations
+echo "Make database migrations"
+python manage.py makemigrations
+
 # Apply database migrations
 echo "Apply database migrations"
 python manage.py migrate
